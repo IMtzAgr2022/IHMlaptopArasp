d979e78a20b908e1de85b673a6c67f6abda79675
IMtzAgr2022 <106554635+IMtzAgr2022@users.noreply.github.com>
2023-07-08 03:46:29 -0600

    Create read-uid.php

---
+++ read-uid.php
@@ -0,0 +1,9 @@
+<?php
+// Obtén el UID de la tarjeta RFID enviado desde la solicitud AJAX
+$uid = $_POST["uid"];
+
+// Realiza cualquier procesamiento adicional necesario con el UID
+
+// Devuelve el UID como respuesta a la solicitud AJAX
+echo $uid;
+?>
